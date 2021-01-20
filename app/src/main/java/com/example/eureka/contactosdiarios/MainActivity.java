4a6a3cb0cde52f35d2461478e4ccfa7f0edead15
acere8 <albertocorreoficial@gmail.com>
2021-01-20 15:07:35 +0100

    toolbar iconos

--- app/src/main/java/com/example/eureka/contactosdiarios/MainActivity.java
+++ app/src/main/java/com/example/eureka/contactosdiarios/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.eureka.contactosdiarios;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.eureka.contactosdiarios.Pojo.Contacto;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar toolbar;
     RecyclerView recyclerView;
@@ -45,4 +49,20 @@ public class MainActivity extends AppCompatActivity {
         contactoList.add(new Contacto("Manolo","55667788"));
         contactoList.add(new Contacto("Manolo","55667788"));
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setEnabled(false);
+        setMenuItemColor(menuItem);
+        return true;
+    }
+
 }
