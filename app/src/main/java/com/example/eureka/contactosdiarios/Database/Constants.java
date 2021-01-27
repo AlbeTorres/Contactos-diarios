fb423e411eb7a6558f810b714fc63a2167027106
acere8 <albertocorreoficial@gmail.com>
2021-01-27 16:02:52 +0100

    Database creada

---
+++ app/src/main/java/com/example/eureka/contactosdiarios/Database/Constants.java
@@ -0,0 +1,22 @@
+package com.example.eureka.contactosdiarios.Database;
+
+/**
+ * Created by Eureka on 27/01/2021.
+ */
+
+public class Constants {
+
+    public static final String DATABASE_NAME = "contactos.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_CONTACTO = "Contacto";
+
+    //columnas tabla contacto
+
+    public static final String ID_CONTACTO = "id_contacto";
+    public static final String NOMBRE = "nombre";
+    public static final String TELEFONO = "telefono";
+    public static final String DIA = "dia";
+    public static final String MES = "mes";
+    public static final String AÑO = "año";
+}
---
+++ app/src/main/java/com/example/eureka/contactosdiarios/Database/Database.java
@@ -0,0 +1,44 @@
+package com.example.eureka.contactosdiarios.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Eureka on 27/01/2021.
+ */
+
+public class Database extends SQLiteOpenHelper {
+
+    Context context;
+
+
+    public Database(Context context)  {
+        super(context, Constants.DATABASE_NAME,null, Constants.DATABASE_VERSION);
+        this.context=context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String queryCrearTablaContacto= "CREATE TABLE "+ Constants.TABLE_CONTACTO + "("+
+                Constants.ID_CONTACTO        + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Constants.NOMBRE    + " TEXT, " +
+                Constants.TELEFONO + " TEXT, " +
+                Constants.DIA + " TEXT, " +
+                Constants.MES      + " TEXT" +
+                Constants.AÑO      + " TEXT" +
+                ")";
+
+        db.execSQL(queryCrearTablaContacto);
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_CONTACTO);
+
+    }
+}
